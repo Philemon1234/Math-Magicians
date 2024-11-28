c4eda83d90e77b90a372a0fe219f8a5d21025c36
Philemon1234 <ntivuguruzwaphilemon0@gmail.com>
2024-11-28 06:28:45 +0300

    Created calculator

---
+++ Math-Magicians/src/components/Calculator.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+function Calculator() {
+  return (
+    <div className='container'>
+      <div className="calculator">
+        <div className="display">0</div>
+        <div className="buttons">
+          <button className="button">AC</button>
+          <button className="button">+/-</button>
+          <button className="button">%</button>
+          <button className="button operator">&divide;</button>
+
+          <button className="button">7</button>
+          <button className="button">8</button>
+          <button className="button">9</button>
+          <button className="button operator">&times;</button>
+
+          <button className="button">4</button>
+          <button className="button">5</button>
+          <button className="button">6</button>
+          <button className="button operator">&minus;</button>
+
+          <button className="button">1</button>
+          <button className="button">2</button>
+          <button className="button">3</button>
+          <button className="button operator">+</button>
+
+          <button className="button zero">0</button>
+          <button className="button">.</button>
+          <button className="button operator">=</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Calculator
